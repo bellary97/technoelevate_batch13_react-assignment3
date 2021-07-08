1e405e978f41af6721274a2b8a6d7d62f92314e9
sandeepb <sandeepbellary97@gmail.com>
2021-07-08 09:33:05 +0530

    assignment 3 is added

---
+++ project1/src/App.js
@@ -0,0 +1,24 @@
+/*import logo from './logo.svg';
+import './App.css';
+import Vote from "./components/Vote"
+function App() {
+  return (
+    <div>
+       <Vote/>
+    </div>
+  );
+}*/
+
+
+import logo from './logo.svg';
+import './App.css';
+import Bgcolor from "./components/Bgcolor"
+function App() {
+  return (
+    <div>
+       <Bgcolor/>
+    </div>
+  );
+}
+
+export default App;
